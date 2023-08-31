5f3abab374502d67345f74f94f304eef48459b0c
richard-krv <richard.krv@gmail.com>
2023-08-31 20:50:52 +0300

    Add project files.

---
+++ RcCollections/RcHashSet.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RcCollections
+{
+    internal class RcHashSet<T>
+    {
+        /// <summary>
+        /// Only stores values
+        /// Not sorted
+        /// Dynamic size limited by stack
+        /// Type specific <T>
+        /// </summary>
+        private readonly HashSet<T> _items;
+
+        public RcHashSet()
+        {
+            _items = new HashSet<T>();
+        }
+
+        public T Value => _items.;
+    }
+}
---
+++ RcCollections/User.cs
@@ -0,0 +1,25 @@
+﻿namespace RcCollections
+{
+    internal class User
+    {
+        public required string Name { get; set; }
+
+        public int? Age { get; set; }
+
+        public override bool Equals(object obj!!) => Equals(obj as User);
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(this.Name, this.Age);
+        }
+
+        private bool Equals(User that)
+        {
+            if (that == null)
+            {
+                return false;
+            }
+            return Equals(Name, that.Name) && Age == that.Age;
+        }
+    }
+}
